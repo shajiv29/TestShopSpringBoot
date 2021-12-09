9a54bdbfbcc148b69db544a9388bb43d468577ec
shajiv29 <shajiv29@gmail.com>
2021-12-09 11:02:08 +0600

    First upload in git

---
+++ src/main/java/com/sazib/hardware/TestShopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sazib.hardware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestShopSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestShopSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sazib/hardware/TestShopSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sazib.hardware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShopSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
